0716dc90fb84c439c0c5fba4374c8a35ef5d1015
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2022-07-10 23:39:15 +0200

    hide navigation when scrolling down

--- src/app/components/NavBar/index.tsx
+++ src/app/components/NavBar/index.tsx
@@ -1,31 +1,51 @@
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
 import Logo from '../../svgs/Logo';
 import NavDrawer from '../NavDrawer';
 import { StyledNavBar, StyledLogo, StyledMenu, StyledDrawer } from './styles';
+
+interface Props {
+  children: React.ReactElement;
+}
+
+function HideOnScroll(props: Props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
 
 export const menu = ['About us', 'Services', 'Contact us'];
 
 const NavBar = () => {
   return (
-    <StyledNavBar>
-      <Container>
-        <Toolbar disableGutters>
-          <StyledLogo>
-            <Logo />
-          </StyledLogo>
-          <StyledMenu>
-            {menu.map((menu) => (
-              <Button key={menu}>{menu}</Button>
-            ))}
-          </StyledMenu>
-          <StyledDrawer>
-            <NavDrawer />
-          </StyledDrawer>
-        </Toolbar>
-      </Container>
-    </StyledNavBar>
+    <HideOnScroll>
+      <StyledNavBar>
+        <Container>
+          <Toolbar disableGutters>
+            <StyledLogo>
+              <Logo />
+            </StyledLogo>
+            <StyledMenu>
+              {menu.map((menu) => (
+                <Button key={menu}>{menu}</Button>
+              ))}
+            </StyledMenu>
+            <StyledDrawer>
+              <NavDrawer />
+            </StyledDrawer>
+          </Toolbar>
+        </Container>
+      </StyledNavBar>
+    </HideOnScroll>
   );
 };
 
